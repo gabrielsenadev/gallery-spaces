8c9fd0acd7552b3d228e0595046b58f200d760a3
Gabriel Sena <git@gabrielsena.dev>
2024-05-02 21:30:36 -0300

    feat: (test netlify deploy)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  buildDir: 'dist',
   devtools: { enabled: true },
   nitro: {
-    preset: "netlify",
+    preset: "netlify-edge",
   }
 })
