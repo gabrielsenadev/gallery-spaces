a7686f5a8a3297729108c8d9b35eb611e3021be1
Gabriel Sena <git@gabrielsena.dev>
2024-05-04 00:01:02 -0300

    feat: (add authentication middleware)

---
+++ server/middleware/auth.ts
@@ -0,0 +1,29 @@
+import { AuthService } from "~/server/service";
+
+export default defineEventHandler((event) => {
+  const url = event.node.req.url;
+  if (!url?.startsWith('/api/') || url?.startsWith('/api/auth') && url !== '/api/auth/user') {
+    return;
+  }
+  
+  const authorizationHeader = getRequestHeader(event, 'Authorization');
+  if (authorizationHeader === undefined) {
+    throw createError({
+      statusCode: 401,
+      message: 'Authorization header not found.',
+    });
+  }
+  
+  const [, token] = authorizationHeader.split('Bearer ');
+  
+  const user = AuthService.getInstance().getUserByToken(token);
+  
+  if (!user) {
+    throw createError({
+      statusCode: 401,
+      message: 'Invalid user token.',
+    });
+  }
+
+  event.context.user = user;
+})
